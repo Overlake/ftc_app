265499b4f4f39c6b72205062137102675c19e819
Nikhil Gupta-Iwasaki <ngupta-iwasaki@hotmail.com>
2015-11-22 11:08:44 -0800

    Encoder test changes

--- Overlake/src/main/java/org/Overlake/ftc/Team_7330/EncoderTest.java
+++ Overlake/src/main/java/org/Overlake/ftc/Team_7330/EncoderTest.java
@@ -28,10 +28,10 @@ public class EncoderTest extends SynchronousOpMode
         parameters.accelunit = IBNO055IMU.ACCELUNIT.METERS_PERSEC_PERSEC;
         parameters.loggingEnabled = true;
         parameters.loggingTag = "BNO055";
-        // imu = ClassFactory.createAdaFruitBNO055IMU(hardwareMap.i2cDevice.get("imu"), parameters);
+        imu = ClassFactory.createAdaFruitBNO055IMU(hardwareMap.i2cDevice.get("imu"), parameters);
 
         // Enable reporting of position using the naive integrator
-        // imu.startAccelerationIntegration(new Position(), new Velocity());
+        imu.startAccelerationIntegration(new Position(), new Velocity());
 
         // Initialize our hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names you assigned during the robot configuration
@@ -40,139 +40,21 @@ public class EncoderTest extends SynchronousOpMode
         this.motorBackRight = this.hardwareMap.dcMotor.get("motorBackRight");
         this.motorFrontLeft = this.hardwareMap.dcMotor.get("motorFrontLeft");
         this.motorBackLeft = this.hardwareMap.dcMotor.get("motorBackLeft");
-        telemetry.update();
+        // telemetry.update();
 
         // One of the two motors (here, the left) should be set to reversed direction
         // so that it can take the same power level values as the other motor.
         this.motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         this.motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
 
-        while (this.motorFrontRight.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-                this.motorBackRight.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-                this.motorFrontLeft.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-                this.motorBackLeft.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS)
+        try
         {
-            this.motorFrontRight.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorBackRight.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorFrontLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorBackLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+            turn(90,.5);
         }
-
-        double ticks = 5000;
-        double rightPower = 0.8;
-        double leftPower = 0.8;
-        driveWithEncoders(ticks, rightPower, leftPower, false);
-    }
-
-    void driveWithEncoders(double distance, double rightPower, double leftPower, boolean useIMU)
-    {
-        telemetry.update();
-
-        while (this.motorFrontRight.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-               this.motorBackRight.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-               this.motorFrontLeft.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-               this.motorBackLeft.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS)
+        catch (Exception e)
         {
-            this.motorFrontRight.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorBackRight.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorFrontLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorBackLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+            throw new EmptyStackException();
         }
-
-        telemetry.update();
-
-        while (this.motorFrontRight.getChannelMode() != DcMotorController.RunMode.RUN_TO_POSITION &&
-                this.motorBackRight.getChannelMode() != DcMotorController.RunMode.RUN_TO_POSITION &&
-                this.motorFrontLeft.getChannelMode() != DcMotorController.RunMode.RUN_TO_POSITION &&
-                this.motorBackLeft.getChannelMode() != DcMotorController.RunMode.RUN_TO_POSITION)
-        {
-            this.motorFrontRight.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
-            this.motorBackRight.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
-            this.motorFrontLeft.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
-            this.motorBackLeft.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        }
-
-        telemetry.update();
-
-        int tempDistance;
-        if (useIMU)
-        {
-            tempDistance = Integer.MAX_VALUE;
-        }
-        else
-        {
-            tempDistance = (int)distance;
-        }
-
-        telemetry.update();
-
-        while (this.motorFrontRight.getTargetPosition() != tempDistance &&
-               this.motorBackRight.getTargetPosition() != tempDistance &&
-               this.motorFrontLeft.getTargetPosition() != tempDistance &&
-               this.motorBackLeft.getTargetPosition() != tempDistance)
-        {
-            this.motorFrontRight.setTargetPosition(tempDistance);
-            this.motorBackRight.setTargetPosition(tempDistance);
-            this.motorFrontLeft.setTargetPosition(tempDistance);
-            this.motorBackLeft.setTargetPosition(tempDistance);
-        }
-
-        telemetry.update();
-
-        while (this.motorFrontRight.getPower() != rightPower &&
-                this.motorBackRight.getPower() != rightPower &&
-                this.motorFrontLeft.getPower() != leftPower &&
-                this.motorBackLeft.getPower() != leftPower)
-        {
-            this.motorFrontRight.setPower(rightPower);
-            this.motorBackRight.setPower(rightPower);
-            this.motorFrontLeft.setPower(leftPower);
-            this.motorBackLeft.setPower(leftPower);
-        }
-
-        telemetry.update();
-
-        if (useIMU)
-        {
-            // TODO: is this x or y?
-            while (imu.getPosition().y < distance)
-            {
-            }
-        }
-        else
-        {
-            while (this.motorFrontRight.isBusy() && this.motorBackRight.isBusy() &&
-                   this.motorFrontLeft.isBusy() && this.motorBackLeft.isBusy()) {
-            }
-        }
-
-        telemetry.update();
-
-        while (this.motorFrontRight.getPower() != 0 &&
-                this.motorBackRight.getPower() != 0 &&
-                this.motorFrontLeft.getPower() != 0 &&
-                this.motorBackLeft.getPower() != 0)
-        {
-            this.motorFrontRight.setPower(0);
-            this.motorBackRight.setPower(0);
-            this.motorFrontLeft.setPower(0);
-            this.motorBackLeft.setPower(0);
-        }
-
-        telemetry.update();
-
-        while (this.motorFrontRight.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-                this.motorBackRight.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-                this.motorFrontLeft.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS &&
-                this.motorBackLeft.getChannelMode() != DcMotorController.RunMode.RESET_ENCODERS)
-        {
-            this.motorFrontRight.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorBackRight.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorFrontLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-            this.motorBackLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-        }
-
-        telemetry.update();
     }
 
     // to turn right, degrees and power should both be positive
