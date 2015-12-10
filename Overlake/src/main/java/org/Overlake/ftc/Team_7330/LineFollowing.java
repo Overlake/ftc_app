66f66154ff75b46680da936529006667ea31d7bf
rohanjha <rj16@live.com>
2015-12-09 16:39:33 -0800

    Line Following

--- Overlake/src/main/java/org/Overlake/ftc/Team_7330/LineFollowing.java
+++ Overlake/src/main/java/org/Overlake/ftc/Team_7330/LineFollowing.java
@@ -4,47 +4,41 @@ package org.Overlake.ftc.Team_7330;
  * Created by Xiao on 11/8/2015.
  */
 public class LineFollowing {
-    /////this is for red team currently
+
     public enum Color {red, blue, white,}
     public enum Side {left, right}
 
-    public boolean isColor (Color color, Side side)
+    /*Ruthie's ideas for how this is all going to work
+        1)drive straight most of the way to the parking zone, quickly
+        2)while(don't see our team's color) drive slowly
+        3)after we find our blue/red line:
+            while(don't see white) follow our team's color's line
+        4)after we find the white line:
+            a)follow white line for an undetermined
+                OR:
+            b)use imu to turn to face buttons directly
+        4)do something to approach and push buttons
+    */
+
+    public boolean isColor (Color color, Side sensorSide, Side colorSide)
     {
-       /* return (color==Color.white&& Values in Range||
-                    color==Color.blue&& values in range||
-                    color==Color.red&& values in range)*/
+       /*1)find out what the RGB sensor sees
+         2)compare it to a range of values for the given color we are looking for
+         3)return if the sensor is seeing the color we want
+        */
     }
 
-    public void followColor(Color followColor)
+    public void followColor(Color color, Side sensorSide, Side colorSide)
     {
-        if(isColor(followColor,Side.left))
+        if(isColor(color, sensorSide, colorSide))
         {
-            //turn right: increase left wheel power a bit and then move forward
+            //turn towards non-color (opposite of colorSide)
         }
         else
         {
-            //turn left: increase right wheel power a bit and then move forward
+            //turn towards color (same direction as colorSide)
         }
     }
 
-    public void followColorUntil(Color oldColor, Color newColor)
-    {
-        while (!isColor(newColor))
-        {
-            followColor(oldColor);
-        }
-        //stop then follow white
-        followColor(newColor);
-    }
-
-    public void driveStraightUntil(Color newColor)
-    {
-        while (!isColor(newColor))
-        {
-            // drive
-        }
-
-        // set the motors to zero
-    }
 }
 
